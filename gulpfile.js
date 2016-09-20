28aef937a389b514e5462280827872e60612179c
Ronan Brett <Ronan.Brett@theaa.ie>
2016-09-20 23:57:30 +0100

    Update Server

--- gulpfile.js
+++ gulpfile.js
@@ -5,4 +5,11 @@ gulp.task('copy-angular-to-server', function () {
 		.pipe(gulp.dest('infographics-server/angular-dist'))
 })
 
-gulp.task('build', ['copy-angular-to-server'], function () { });
+gulp.task('copy-default-to-server', function () {
+	return gulp.src(
+		['infographics-default/target/production/app.min.js','infographics-default/target/production/src/app.css'])
+		.pipe(gulp.dest('infographics-server/src/public/js/infographic'))
+})
+
+
+gulp.task('build', ['copy-angular-to-server','copy-default-to-server'], function () { });
