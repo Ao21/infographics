b2abc0962e6a8ebfd08ac16432cd577ccb9c870d
Ronan Brett <Ronan.Brett@theaa.ie>
2016-09-21 23:16:16 +0100

    Update infographics-chart

--- gulpfile.js
+++ gulpfile.js
@@ -11,5 +11,12 @@ gulp.task('copy-default-to-server', function () {
 		.pipe(gulp.dest('infographics-server/src/public/js/infographic'))
 })
 
+gulp.task('copy-charts-to-server', function () {
+	return gulp.src(
+		['infographics-chart/target/production/app.min.js',
+			'infographics-chart/target/production/src/app.css'])
+		.pipe(gulp.dest('infographics-server/src/public/js/graphs'))
+})
 
-gulp.task('build', ['copy-angular-to-server','copy-default-to-server'], function () { });
+
+gulp.task('build', ['copy-angular-to-server','copy-default-to-server','copy-charts-to-server'], function () { });
